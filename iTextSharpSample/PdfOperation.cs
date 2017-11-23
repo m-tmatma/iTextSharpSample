b5c53097d031ecb45e6faa7c770c976f64129f95
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-11-23 17:21:24 +0900

    add comment

--- iTextSharpSample/PdfOperation.cs
+++ iTextSharpSample/PdfOperation.cs
@@ -8,6 +8,10 @@ namespace iTextSharpSample
 {
     public class PdfOperation
     {
+        /// <summary>
+        /// Dump PDF Information
+        /// </summary>
+        /// <param name="srcFile">source PDF file path</param>
         public static void ShowPdfInfo(string srcFile)
         {
             using (var reader = new PdfReader(srcFile))
@@ -19,6 +23,13 @@ namespace iTextSharpSample
             }
         }
 
+        /// <summary>
+        /// Set Password to a PDF file
+        /// </summary>
+        /// <param name="srcFile">source PDF file path</param>
+        /// <param name="dstFile">destination PDF file path</param>
+        /// <param name="ownerPassword">owner password</param>
+        /// <param name="userPassword">user password</param>
         public static void SetPassword(string srcFile, string dstFile, string ownerPassword, string userPassword)
         {
             byte[] byteUSER = Encoding.ASCII.GetBytes(userPassword);   // user password
@@ -38,6 +49,13 @@ namespace iTextSharpSample
             }
         }
 
+        /// <summary>
+        /// set PDF information and set password to a PDF file
+        /// </summary>
+        /// <param name="srcFile">source PDF file path</param>
+        /// <param name="dstFile">destination PDF file path</param>
+        /// <param name="ownerPassword">owner password</param>
+        /// <param name="userPassword">user password</param>
         public static void SetPDFInfoAndEncrypt(string srcFile, string dstFile, string ownerPassword, string userPassword)
         {
             byte[] byteUSER = Encoding.ASCII.GetBytes(userPassword);   // user password
